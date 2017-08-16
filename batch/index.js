986697e968f03e750905eed9800af6c4468801e1
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-08-16 19:28:39 +0900

    add send confirm template message

--- batch/index.js
+++ batch/index.js
@@ -1,5 +1,5 @@
 const { LineBot } = require('@noriaki/linebot/lib/LineBot');
-const { TextMessage } = require('@noriaki/linebot/lib/LineMessages');
+const { TemplateMessage } = require('@noriaki/linebot/lib/LineMessages');
 const { createConnection } = require('../commons/db');
 const { userSchema } = require('../commons/model');
 
@@ -17,8 +17,24 @@ const User = connection.model('User', userSchema);
 (async () => {
   const users = await User.find({ active: true });
   for (const user of users) {
-    const message = new TextMessage({
-      text: '明日は週刊少年マガジン発売の日！',
+    const message = new TemplateMessage({
+      altText: '申し訳ありませんが非対応です。LINEバージョン6.7.0以上をご利用ください',
+      template: {
+        type: 'confirm',
+        text: 'はじめの一歩今週お休みだよ！読んでる？',
+        actions: [
+          {
+            type: 'message',
+            label: 'はい',
+            text: '読んでるよ！',
+          },
+          {
+            type: 'message',
+            label: 'いいえ',
+            text: 'いまは読んでないなー',
+          },
+        ],
+      },
     });
     await client.push(user.identifier, message);
     console.log('[send push] UserId: %s', user.identifier);
